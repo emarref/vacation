4d2887a8c6c2e0014eb0ad9667517ab9d2ac2f47
Malcolm Fell <emarref@gmail.com>
2015-02-12 09:53:22 +1300

    Parse url parameters in prefix

--- src/Emarref/Vacation/Controller/EndpointMatcher.php
+++ src/Emarref/Vacation/Controller/EndpointMatcher.php
@@ -20,14 +20,20 @@ class EndpointMatcher implements EndpointMatcherInterface
         $this->prefix = $prefix;
     }
 
-    /**
-     * @param RequestInterface  $request
-     * @param Metadata\Endpoint $endpointMetadata
-     * @return boolean
-     */
-    public function matches(RequestInterface $request, Metadata\Endpoint $endpointMetadata)
+    protected function normalisePath($path)
     {
-        $pathSections = explode('/', trim($endpointMetadata->path, '/'));
+        return trim($path, '/');
+    }
+
+    /**
+     * @param RequestInterface $request
+     * @param string           $path
+     * @throws \RuntimeException
+     * @return string
+     */
+    protected function resolvePath(RequestInterface $request, $path)
+    {
+        $pathSections = explode('/', $path);
 
         // Substitute parameter placeholders with their values to match request URI
         foreach ($pathSections as &$section) {
@@ -36,18 +42,35 @@ class EndpointMatcher implements EndpointMatcherInterface
                 if ($parameter = $request->getParameter($parameterName)) {
                     $section = $parameter;
                 } else {
-                    return false;
+                    throw new \RuntimeException(sprintf('Required path parameter "%s" not found.', $parameterName));
                 }
             }
         }
 
-        $controllerPath = trim(implode('/', $pathSections), '/');
-        $requestPath    = trim(parse_url($request->getUrl(), PHP_URL_PATH), '/');
+        return implode('/', $pathSections);
+    }
 
-        if (trim(sprintf('%s/%s', trim($this->prefix, '/'), $controllerPath), '/') === $requestPath) {
-            return true;
+    /**
+     * @param RequestInterface  $request
+     * @param Metadata\Endpoint $endpointMetadata
+     * @return boolean
+     */
+    public function matches(RequestInterface $request, Metadata\Endpoint $endpointMetadata)
+    {
+        $path = sprintf(
+            '%s/%s',
+            $this->normalisePath($this->prefix),
+            $this->normalisePath($endpointMetadata->path)
+        );
+
+        try {
+            $controllerPath = $this->resolvePath($request, $path);
+        } catch (\RuntimeException $e) {
+            return false;
         }
 
-        return false;
+        $requestPath = trim(parse_url($request->getUrl(), PHP_URL_PATH), '/');
+
+        return $controllerPath === $requestPath;
     }
 }
